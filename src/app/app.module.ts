6ce2703cabcf6e1aad3d2297d54ae035e8b95130
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-25 15:32:17 -0300

    technical-list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,9 +31,10 @@ import { MatCardModule } from "@angular/material/card";
 import { NavComponent } from "./components/nav/nav.component";
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, HeaderComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, HeaderComponent, TecnicoListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
