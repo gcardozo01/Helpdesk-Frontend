cd2b854426c98a0bc3ed8b938f321245cf418a81
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-10-31 15:43:57 -0300

    route to technical-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NavComponent } from "./components/nav/nav.component";
 import { HomeComponent } from "./components/home/home.component";
 import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
 import { LoginComponent } from "./components/login/login.component";
+import { TecnicoCreateComponent } from "./components/tecnico/tecnico-create/tecnico-create.component";
 
 // Guard
 import { AuthGuard } from "./auth/auth.guard";
@@ -20,6 +21,10 @@ const routes: Routes = [
       {
         path: "tecnicos",
         component: TecnicoListComponent,
+      },
+      {
+        path: "tecnicos/create",
+        component: TecnicoCreateComponent,
       },
     ],
   },
