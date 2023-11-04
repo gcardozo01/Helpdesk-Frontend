3f1663add9d323bea496a37d939b12d7485078ab
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-04 15:49:29 -0300

    echnical-delete component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,13 +34,15 @@ import { HeaderComponent } from "./components/header/header.component";
 import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
 import { LoginComponent } from "./components/login/login.component";
 import { ToastrModule } from "ngx-toastr";
+import { TecnicoUpdateComponent } from "./components/tecnico/tecnico-update/tecnico-update.component";
+import { TecnicoDeleteComponent } from "./components/tecnico/tecnico-delete/tecnico-delete.component";
+
 // Interceptor
 import { AuthInterceptorProvider } from "./interceptors/auth.interceptor";
 import { TecnicoCreateComponent } from "./components/tecnico/tecnico-create/tecnico-create.component";
 
 // Mask
 import { NgxMaskModule } from "ngx-mask";
-import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 @NgModule({
   declarations: [
@@ -52,6 +54,7 @@ import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecn
     LoginComponent,
     TecnicoCreateComponent,
     TecnicoUpdateComponent,
+    TecnicoDeleteComponent,
   ],
   imports: [
     BrowserModule,
