ee1f17d9a1748969ec18e3174cc9f006133a5342
Guilherme Cardozo <guilhermecardozo91@gmail.com>
2023-11-14 14:28:33 -0300

    HTML and CSS for called-read

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+
+// Models
+import { Chamado } from "src/app/models/chamado";
+
+// Services
+import { ChamadoService } from "src/app/services/chamado.service";
 
 @Component({
-  selector: 'app-chamado-read',
-  templateUrl: './chamado-read.component.html',
-  styleUrls: ['./chamado-read.component.css']
+  selector: "app-chamado-read",
+  templateUrl: "./chamado-read.component.html",
+  styleUrls: ["./chamado-read.component.css"],
 })
 export class ChamadoReadComponent implements OnInit {
+  chamado: Chamado = {
+    prioridade: "",
+    status: "",
+    titulo: "",
+    observacoes: "",
+    tecnico: "",
+    cliente: "",
+    nomeCliente: "",
+    nomeTecnico: "",
+  };
 
-  constructor() { }
+  constructor(
+    private chamadoService: ChamadoService,
+    private toast: ToastrService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get("id");
+    this.findById();
   }
 
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(
+      (res) => {
+        this.chamado = res;
+      },
+      (err) => {
+        this.toast.error(err.error.error);
+      }
+    );
+  }
+
+  retornaStatus(status: any): string {
+    if (status == "0") {
+      return "ABERTO";
+    } else if (status == "1") {
+      return "EM ANDAMENTO";
+    } else {
+      return "ENCERRADO";
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == "0") {
+      return "BAIXA";
+    } else if (prioridade == "1") {
+      return "MÉDIA";
+    } else {
+      return "ALTA";
+    }
+  }
 }
